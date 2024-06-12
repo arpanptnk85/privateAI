b51429bc50773a452bc660cce3338a4b29a6e637
Arpan Patnaik <arpan.patnaik@hyperkurve.com>
2024-06-12 22:03:42 +0530

    feat: database setup

--- backend/privateai/settings.py
+++ backend/privateai/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'privateai.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'privateai_db'),  # Use environment variable
+        'USER': os.environ.get('POSTGRES_USER', 'privateai_user'),    # Use environment variable
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'colorsandowlbook'),  # Use environment variable
+        'HOST': 'localhost',  # Use container name of the Postgres container
+        'PORT': 5432,
     }
 }
 
