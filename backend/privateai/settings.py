1ff5af0e242f90c496489fa0b10e2c1ed79fe462
Arpan Patnaik <arpan.patnaik85@gmail.com>
2024-06-12 22:10:05 +0530

    feat: API app framework added

--- backend/privateai/settings.py
+++ backend/privateai/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # Add rest_framework to the list of installed apps
 ]
 
 MIDDLEWARE = [
