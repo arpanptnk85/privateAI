decf3e74c67db4dce7fbd0baf57d6845bd44dcdc
Arpan Patnaik <arpan.patnaik85@gmail.com>
2024-06-14 00:24:07 +0530

    Update settings.py
    Comment added

--- backend/privateai/settings.py
+++ backend/privateai/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+# Loads the environment variables
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
